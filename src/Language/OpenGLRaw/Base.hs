70d46e793190d792957e184e464ac883e6fc2e65
Lars Corbijn <aspergesoepje@gmail.com>
2013-03-13 18:50:24 +0100

    Adds ftype from openGLRawgen.

--- src/Language/OpenGLRaw/Base.hs
+++ src/Language/OpenGLRaw/Base.hs
@@ -1,5 +1,19 @@
 module Language.OpenGLRaw.Base (
+    GLName, FType(..),
+
     Category(..), Extension(..),
 ) where
 
 import Text.OpenGL.Spec(Category(..), Extension(..))
+
+-- | The original name of something from OpenGL (thus the name as used in the
+-- specification).
+type GLName = String
+
+-- | Simple typing, sufficient for OpenGL functions.
+data FType
+    = TCon String
+    | TVar
+    | TPtr FType
+    | UnitTCon
+    deriving (Eq, Ord, Show)
