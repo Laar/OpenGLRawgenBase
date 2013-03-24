a1555907344df1e4df9aa2d80f5895d5960094f1
Lars Corbijn <aspergesoepje@gmail.com>
2013-03-24 16:32:08 +0100

    Adds simple serialization

---
+++ OpenGLRawInterface/src/Language/OpenGLRaw/Interface/Serialize.hs
@@ -0,0 +1,35 @@
+module Language.OpenGLRaw.Interface.Serialize (
+    writePackage, writeModule,
+) where
+
+import System.Directory
+import System.FilePath
+import Text.XML.Light
+
+import Language.OpenGLRaw.Interface.Types
+import Language.OpenGLRaw.Interface.Xml
+
+writePackage :: FilePath -> OpenGLRawI -> IO ()
+writePackage path package =
+    let path' = path </> "package" <.> "hs"
+        xmlPackage = toGLXml package
+    in safeWriteFile path' $ showTopElement xmlPackage
+
+writeModule :: FilePath -> ModuleI -> IO ()
+writeModule path m =
+    let path' = path </> "modules" </> moduleNameToPath (modName m) <.> "xml"
+        xmlModule = toGLXml m
+    in safeWriteFile path' $ showTopElement xmlModule
+
+-- copy from CodeGenerating
+-- | Converts the module name to the path of it's source code.
+moduleNameToPath :: ModuleName -> FilePath
+moduleNameToPath (ModuleName n) = foldr replace [] n
+    where
+        replace '.' p = pathSeparator : p
+        replace c p = c : p
+
+-- copy from OpenGLRawgen
+safeWriteFile :: FilePath -> String -> IO ()
+safeWriteFile fp fc = createDirectoryIfMissing True (dropFileName fp)
+     >> writeFile fp fc
